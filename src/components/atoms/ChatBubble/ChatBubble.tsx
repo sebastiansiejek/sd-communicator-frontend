0994ed683e9738d2b60fcae60ce27c66186e8406
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-11-28 14:32:44 +0100

    feat: add ChatBubble

---
+++ src/components/atoms/ChatBubble/ChatBubble.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+
+type IProps = {
+  isSender: boolean
+}
+
+const ChatBubbleStyled = styled.div<IProps>`
+  padding: 1rem 1.2rem;
+  border-radius: 1rem;
+  width: max-content;
+  max-width: 90%;
+
+  ${({ isSender }) =>
+    isSender &&
+    css`
+      background-color: ${({ theme }) => theme.colors.blue[600]};
+    `}
+
+  ${({ isSender }) =>
+    !isSender &&
+    css`
+      background-color: ${({ theme }) => theme.colors.yellow[500]};
+      align-self: flex-end;
+      text-align: right;
+    `}
+`
+
+const ChatBubble: React.FC<IProps> = ({ children, isSender }) => {
+  return <ChatBubbleStyled isSender={isSender}>{children}</ChatBubbleStyled>
+}
+
+export default ChatBubble
