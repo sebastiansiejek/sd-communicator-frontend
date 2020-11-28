c645bc6adc677473c9dba2615fe32110f3917406
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-11-28 17:27:15 +0100

    chore: remove App div and change with arrow function

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import Chat from './components/Chat/Chat'
+import GlobalStyles from './theme/GlobalStyles'
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './theme/theme'
-import GlobalStyles from './theme/GlobalStyles'
 
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <div className="App">
-        <Chat />
-      </div>
-    </ThemeProvider>
-  )
-}
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Chat />
+  </ThemeProvider>
+)
 
 export default App
