1e948935d88796c89662d77c2578330639bc25d5
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-11-28 22:15:35 +0100

    feat: add roomId

--- src/components/Chat/ChatRoom/ChatRoom.tsx
+++ src/components/Chat/ChatRoom/ChatRoom.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { useDispatch, connect } from 'react-redux'
+import { setRoomId } from 'store/slices/userSlice'
+import { IStore } from 'store/store'
 
 interface Props {
   roomId?: string
 }
 
+interface IInputs {
+  room_id: string
+}
+
 const ChatRoom: React.FC<Props> = ({ roomId }) => {
   const { handleSubmit, register, reset } = useForm()
+  const dispatch = useDispatch()
 
   return (
     <>
       {!roomId && (
         <form
-          onSubmit={handleSubmit(() => {
+          onSubmit={handleSubmit(({ room_id }: IInputs) => {
+            dispatch(setRoomId(room_id))
             reset({
               room_id: ''
             })
@@ -28,8 +37,17 @@ const ChatRoom: React.FC<Props> = ({ roomId }) => {
           <button type="submit">Join/create</button>
         </form>
       )}
+      {roomId && (
+        <p>
+          Your room id: <strong>{roomId}</strong>
+        </p>
+      )}
     </>
   )
 }
 
-export default ChatRoom
+export default connect((state: IStore) => {
+  return {
+    roomId: state.user.roomId
+  }
+})(ChatRoom)
