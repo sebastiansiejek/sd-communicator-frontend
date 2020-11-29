dce0fe977480fffcb08f30d984a684195406f529
Sebastian Siejek <contact@sebastiansiejek.dev>
2020-11-29 11:10:39 +0100

    feat: add button with generate room id

--- src/components/Chat/ChatRoom/ChatRoom.tsx
+++ src/components/Chat/ChatRoom/ChatRoom.tsx
@@ -1,11 +1,12 @@
 import ButtonStyled from 'components/atoms/Button/Button'
 import Input from 'components/atoms/Input'
-import React from 'react'
-import { useForm } from 'react-hook-form'
-import { useDispatch, connect } from 'react-redux'
-import { setRoomId } from 'store/slices/userSlice'
-import { IStore } from 'store/store'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { IStore } from 'store/store'
+import { nanoid } from 'nanoid'
+import { setRoomId } from 'store/slices/userSlice'
+import { useDispatch, connect } from 'react-redux'
+import { useForm } from 'react-hook-form'
 
 interface Props {
   roomId?: string
@@ -21,28 +22,42 @@ const ChatRoomFormStyled = styled.form`
 
 const ChatRoom: React.FC<Props> = ({ roomId }) => {
   const { handleSubmit, register, reset } = useForm()
+  const [generatedId, setGeneratedId] = useState('')
   const dispatch = useDispatch()
 
   return (
     <>
       {!roomId && (
-        <ChatRoomFormStyled
-          onSubmit={handleSubmit(({ room_id }: IInputs) => {
-            dispatch(setRoomId(room_id))
-            reset({
-              room_id: ''
-            })
-          })}
-        >
-          <Input
-            name="room_id"
-            type="text"
-            ref={register}
-            placeholder="Room id"
-            required
-          />
-          <ButtonStyled type="submit">Join/create</ButtonStyled>
-        </ChatRoomFormStyled>
+        <>
+          <ChatRoomFormStyled
+            onSubmit={handleSubmit(({ room_id }: IInputs) => {
+              dispatch(setRoomId(room_id))
+              reset({
+                room_id: ''
+              })
+            })}
+          >
+            <Input
+              name="room_id"
+              type="text"
+              ref={register}
+              placeholder="Room id"
+              defaultValue={generatedId}
+              required
+            />
+            <ButtonStyled type="submit">Join/create</ButtonStyled>
+          </ChatRoomFormStyled>
+          <ButtonStyled
+            style={{
+              marginTop: '.8rem'
+            }}
+            onClick={() => {
+              setGeneratedId(nanoid())
+            }}
+          >
+            Generate unique room id
+          </ButtonStyled>
+        </>
       )}
       {roomId && (
         <p>
