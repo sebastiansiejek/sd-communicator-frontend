991c80f7142cdd9ca67b88565a21a8c13b1f06d8
Sebastian Siejek <contact@sebastiansiejek.dev>
2021-04-22 20:08:13 +0200

    feat: set video

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, createRef } from 'react'
+import { toast } from 'react-toastify'
 import styled from 'styled-components'
 
 const VideoStyled = styled.video``
 
 const Video: React.FC = () => {
-  return <VideoStyled />
+  const videoRef = createRef<HTMLVideoElement>()
+
+  useEffect(() => {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: true,
+        video: {
+          width: { ideal: 4096 },
+          height: { ideal: 2160 }
+        }
+      })
+      .then((stream) => {
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream
+          videoRef.current.onloadedmetadata = function (e) {
+            videoRef.current?.play()
+          }
+        }
+      })
+      .catch((error) => {
+        console.warn(error)
+        toast.error(error)
+      })
+  })
+
+  return <VideoStyled ref={videoRef} />
 }
 
 export default Video
